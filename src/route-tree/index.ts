3eedafb3987cd246637a6fff3968e8e134c8b956
Vartan <vartan.bg@gmail.com>
2023-07-31 20:43:02 +0400

    fix

--- src/route-tree/index.ts
+++ src/route-tree/index.ts
@@ -5,7 +5,7 @@ import * as path from 'path';
 export function routeTree(options: any): Rule {
   return (tree: Tree, context: SchematicContext) => {
     // Get the path to the routes-routing.module file
-    const routeModulePath = path.join(options.path, 'routes-routing.module.ts');
+    const routeModulePath = path.join(options.path || '', 'routes-routing.module.ts');
 
     // Read the file content
     const content = tree.read(routeModulePath);
