9038ec9f3f685df0b785d7c1dc35166178077357
Vartan <vartan.bg@gmail.com>
2023-07-31 20:46:29 +0400

    fix

--- src/route-tree/index.ts
+++ src/route-tree/index.ts
@@ -3,9 +3,14 @@ import { parse as parseJson } from 'json5';
 import * as path from 'path';
 
 export function routeTree(options: any): Rule {
+  // Set a default value for the "path" option if it's not provided
+  const defaultPath = 'src/app';
+
+  // Use the provided "path" option or the default value
+  const selectedPath = options && options.path ? options.path : defaultPath;
   return (tree: Tree, context: SchematicContext) => {
     // Get the path to the routes-routing.module file
-    const routeModulePath = path.join(options.path || '', 'routes-routing.module.ts');
+    const routeModulePath = path.join(selectedPath, 'routes-routing.module.ts');
 
     // Read the file content
     const content = tree.read(routeModulePath);
