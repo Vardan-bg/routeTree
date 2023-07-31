2068e6360af73eaeb955500b097cd0095462bd12
Vartan <vartan.bg@gmail.com>
2023-07-31 21:00:39 +0400

    fix

--- src/route-tree/index.js
+++ src/route-tree/index.js
@@ -4,9 +4,13 @@ exports.routeTree = void 0;
 const json5_1 = require("json5");
 const path = require("path");
 function routeTree(options) {
+    // Set a default value for the "path" option if it's not provided
+    const defaultPath = 'src/app';
+    // Use the provided "path" option or the default value
+    const selectedPath = options && options.path ? options.path : defaultPath;
     return (tree, context) => {
         // Get the path to the routes-routing.module file
-        const routeModulePath = path.join(options.path, 'routes-routing.module.ts');
+        const routeModulePath = path.join(selectedPath, 'routes-routing.module.ts');
         // Read the file content
         const content = tree.read(routeModulePath);
         if (content) {
